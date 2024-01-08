31450d57afcc2508fda3e5cbd87dfee8b47104cc
leonardoolia <leonardooliaa@gmail.com>
2024-01-08 14:20:59 +0100

    printed the list on the page with "for"

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ const targetElement = document.getElementById('lista-spesa');
 //? 2. Creare array con elementi all'interno
 const listaSpesa = ['Pane', 'Latte', 'Farina', 'Uova', 'Sale', 'Zucchero', 'Frutta', 'Verdura', 'Patate'];
 
+//? 3. Stampare in pagina la lista della spesa
+let listItems = '<ul>';
+
+//! Versione con il for
+for (let i = 0; i < listaSpesa.length; i++) {
+    listItems += `<li>${listaSpesa[i]}</li>`
+};
+
+
+listItems += '</ul>';
+
+targetElement.innerHTML = listItems;
