58d4a00459dcb054e4901ea87f79779e38a1ce16
leonardoolia <leonardooliaa@gmail.com>
2024-01-08 14:20:21 +0100

    got element from html

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 /* Data una lista della spesa (inventatela), stampare sulla pagina (anche brutalmente, basta che si vedano) 
 gli elementi della lista individualmente con un ciclo while. */
 
+//? 1. Recuperare l'elemento dall'html
+const targetElement = document.getElementById('lista-spesa');
+
